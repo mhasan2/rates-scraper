0e989899897d348c8eb16a9a8a81029a4c9bee29
Mohammed Hasan <mohammed.hasan2@wnco.com>
2024-02-16 20:59:21 -0600

    tap tap

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import requests
+import json
+from pprint import pprint
 
-URL = "https://www.taptapsend.com/"
-page = requests.get(URL)
 
-print(page.text)
+def tap_tap_bdt():
+    url = "https://api.taptapsend.com/api/fxRates"
+    headers = {
+        "appian-version": "web/2022-05-03.0",
+        "x-device-id": "web",
+        "x-device-model": "web"
+    }
+    page = requests.get(url, headers=headers)
+
+    page_json = page.json()
+
+    for country in page_json["availableCountries"]:
+        if country["currency"] == "GBP":
+            for corridor in country["corridors"]:
+                if corridor["currency"] == "BDT":
+                    return corridor["fxRate"]
+
+# print(json.dumps(page_json, indent=2))
